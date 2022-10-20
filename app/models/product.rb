85e1c4237932a830197398daa97cd20bdae32eba
danriser <danieljosepalmar@gmail.com>
2022-10-20 12:11:12 -0300

    genre c created

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :brand
   belongs_to :category
+  has_many :carts
   validates :price, presence: :true 
   validates :brand_id, presence: :true
   validates :category_id, presence: :true
