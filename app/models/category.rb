0ac11028e7c04af3d81fc732725b14a25f1a9f80
artistrea <apiravesi@gmail.com>
2022-10-25 20:27:02 -0300

    comentando models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
-    has_many :products
+    # Perfeito. Falta apenas definir o que acontece com os produtos quando uma categoria é deletada:
+    has_many :products, dependent: :destroy # https://blog.techatpower.com/the-dependent-option-for-model-associations-in-rails-3da6f0983e68
     
     validates :name, presence: :true, uniqueness: :true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    # Perfeito. Só lembrando que no futuro é melhor deixar uma gem, como 'devise',
+    # gerenciar o armazenamento de email e senha
     validates :name, presence: :true
     validates :email, presence: :true, uniqueness: :true
     validates :password, presence: :true
