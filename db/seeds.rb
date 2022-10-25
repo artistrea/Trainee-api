125e42247fdf280a3d37e4b9e4db8b310ae41b55
danriser <danieljosepalmar@gmail.com>
2022-10-24 21:26:19 -0300

    finished products controllers definitive

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ Product.create!(id:1,name: "Nontendi Switch",description: "vg console ", price: 
 Product.create!(id:2,name: "Mouse rgb 50000 dpi",description: "shiny mouse" ,price: 200,stock: 45,brand_id:1 ,category_id: 1)
 Product.create!(id:3,name: "Drip shoes",description: "good quality shoes", price: 200,stock: 45,brand_id:1 ,category_id: 1)
 Product.create!(id:4,name: "T-shirt",description: "cool shirt ", price: 200,stock: 45,brand_id:1 ,category_id: 1)
-Product.create!(id:5,name: "Nontendi",description: "oi ", price: 200,stock: 45,brand_id:1 ,category_id: 1)
-Product.create!(id:6,name: "Nontendi",description: " oi" ,price: 200,stock: 45,brand_id:1 ,category_id: 1)
-Product.create!(id:7,name: "Nontendi",description: " oi", price: 200,stock: 45,brand_id:1 ,category_id: 1)
-Product.create!(id:8,name: "Nontendi",description: " oi", price: 200,stock: 45,brand_id:1 ,category_id: 1)
-Product.create!(id:9,name: "Nontendi",description: " oi", price: 200,stock: 45,brand_id:1 ,category_id: 1)
-Product.create!(id:10,name: "Nontendi",description: " oi", price: 200,stock: 45,brand_id:1 ,category_id: 1)
+Product.create!(id:5,name: "Sunglasses",description: "emoji ", price: 25,stock: 50,brand_id:1 ,category_id: 1)
+Product.create!(id:6,name: "Mario Golf ",description: "Fun game" ,price: 200,stock: 45,brand_id:1 ,category_id: 1)
+Product.create!(id:7,name: "Galaxy M 44",description: "Powerfull Phone", price: 200,stock: 45,brand_id:1 ,category_id: 1)
+Product.create!(id:8,name: "Ipear",description: "Stylish Phone", price: 200,stock: 45,brand_id:1 ,category_id: 1)
+Product.create!(id:9,name: "Headphone",description: "Enhance your listening experience", price: 200,stock: 45,brand_id:1 ,category_id: 1)
+Product.create!(id:10,name: "Pro Controller",description: "Made for pro gamers", price: 200,stock: 45,brand_id:1 ,category_id: 1)
 
 
 
